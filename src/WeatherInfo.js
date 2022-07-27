b57c0863ef14d7694ecaa4a2274b6235f0319af6
Olya <ponomaryova.olya.y@gmail.com>
2022-07-27 13:50:11 +0200

    Add unit conversion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import FormattedDate from "./FormattedDate";
 
 export default function WeatherInfo(props) {
+  let [unit, setUnit] = useState("celsius");
+
+  function changeUnit(newUnit) {
+    setUnit(newUnit);
+  }
+
+  function showTemperature() {
+    let result;
+    if (unit === "celsius") {
+      result = props.weather.temperature;
+    } else {
+      result = (props.weather.temperature * 9) / 5 + 32;
+    }
+    return Math.round(result);
+  }
+
   return (
     <div className="WeatherInfo">
       <h2 className="weather-subheading">
@@ -18,17 +34,31 @@ export default function WeatherInfo(props) {
         <div className="col-4 text-end">
           <img src={props.weather.icon} alt={props.weather.description} />
         </div>
-
         <div className="col-4 current-temperature text-end">
-          {Math.round(props.weather.temperature)}
+          {showTemperature()}
         </div>
-
         <div className="col-4 unit-buttons text-start">
-          <button type="button" className="btn btn-link degree-active">
+          <button
+            type="button"
+            className={
+              "btn btn-link " + (unit === "celsius" ? "degree-active" : "")
+            }
+            onClick={function () {
+              changeUnit("celsius");
+            }}
+          >
             °C
           </button>
           <span>|</span>
-          <button type="button" className="btn btn-link">
+          <button
+            type="button"
+            className={
+              "btn btn-link " + (unit === "fahrenheit" ? "degree-active" : "")
+            }
+            onClick={function () {
+              changeUnit("fahrenheit");
+            }}
+          >
             °F
           </button>
         </div>
