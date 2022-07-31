238dc3c1c9fbb21277b3ee7875e14fd52b9c80fa
Olya <ponomaryova.olya.y@gmail.com>
2022-07-31 16:34:28 +0200

    Delete unused files

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -2,7 +2,6 @@ import React from "react";
 
 export default function WeatherIcon(props) {
   function getIconUrl() {
-    console.log("getIconUrl", props.icon);
     return `http://openweathermap.org/img/wn/${props.icon}@2x.png`;
   }
 
